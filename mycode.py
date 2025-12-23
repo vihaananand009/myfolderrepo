330faca9f3f5a0a22409710beea532dad8f041dd
anandraj12901@gmail.com <anandraj12901@gmail.com>
2025-12-23 12:24:42 +0530

    first launch

--- mycode.py
+++ mycode.py
@@ -1,4 +1,13 @@
-a = 10
-b=20
-s=a+b
-print("sum ",s)
+marks=int (input ("ENTER THE NUMBER THAT STUDENT OBTAINED :\n"))
+if marks >=33 and marks < 45 :
+    grade="D"
+elif marks >=45 and marks <60:
+        grade="C"
+elif marks >=60 and marks <75:
+      grade ="B"
+elif marks >=75 and marks <100:
+      grade ="A"
+else:
+      grade="F"
+print( "YOU OBTAINED ='" +grade +"' GRADE")
+        
