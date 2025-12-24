2b0afbe023acf3fdffa5ff771fd4c0f35ab6cec7
anandraj12901@gmail.com <anandraj12901@gmail.com>
2025-12-24 12:19:41 +0530

    my second priogram

---
+++ sum.py
@@ -0,0 +1,8 @@
+# SUM OF TWO NUMBER TAKING INPUT FROM us
+print("ENTER THE NUMBER 1")
+num1= int(input())
+print("ENTER THE NUMBER 2")
+num2 =int (input ())
+
+sum=num1+num2
+print ("YOUR RESULT IS =",sum)
